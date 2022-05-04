9698b65dc790d0c34df0b0e901a4576a881c4f5f
Alex Matos <alex.matos.freitas@gmail.com>
2022-05-04 14:46:28 -0300

    server: refactor server file
    add routes file and move logic from server file to routes file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
+import { routes } from './routes';
+
 const app = express();
 
-app.get('/', (_, res) => {
-	res.send('Hello!');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
 	console.log('Server started on port 3333');
