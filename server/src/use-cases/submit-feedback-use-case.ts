2cad23ebc5b1f184208e1f2b21f45581c291ce1f
Alex Matos <alex.matos.freitas@gmail.com>
2022-05-04 14:43:03 -0300

    server: add submit feedback use case

---
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -0,0 +1,41 @@
+import { MailAdapter } from '../adapters/mail-adapter';
+import { FeedbacksRepository } from '../repositories/feedbacks-repository';
+
+interface SubmitFeedbackUseCaseRequest {
+	type: string;
+	comment: string;
+	screenshot?: string;
+}
+
+export class SubmitFeedbackUseCase {
+	constructor(
+		private feedbackRepository: FeedbacksRepository,
+		private mailAdapter: MailAdapter,
+	) {}
+
+	async execute({ type, comment, screenshot }: SubmitFeedbackUseCaseRequest) {
+		if (!type) {
+			throw new Error('Type is required');
+		}
+
+		if (!comment) {
+			throw new Error('Comment is required');
+		}
+
+		if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+			throw new Error('Invalid screenshot format.');
+		}
+
+		await this.feedbackRepository.create({ type, comment, screenshot });
+
+		await this.mailAdapter.sendMail({
+			subject: 'New feedback',
+			body: [
+				`<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+				`<p>Feedback type: ${type}</p>`,
+				`<p>Comment: ${comment}</p>`,
+				`</div>`,
+			].join('\n'),
+		});
+	}
+}
