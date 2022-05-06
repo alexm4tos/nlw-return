7c90164d158c5dfca2fce83aaeb1538702708078
Alex Matos <alex.matos.freitas@gmail.com>
2022-05-06 14:00:52 -0300

    web: update env BACKEND_URL variable

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = import.meta.env.BACKEND_URL || 'http://localhost:3333';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3333';
 
 export const api = axios.create({
 	baseURL: BACKEND_URL,
